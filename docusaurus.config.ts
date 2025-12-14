49dd81ef40d8efdab09db27cd4fd6e4983e49fd3
Ayeha Qadir7 <ayeshaabdulqadir07@gmail.com>
2025-12-15 02:18:26 +0500

    vercel url fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://AyeshaQadir7.github.io",
+  url: "https://physical-ai-robotics.vercel.app/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/physical-ai-robotics-textbook/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
